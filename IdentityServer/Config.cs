c7dcffb8acbd1d37606cecad5a3193b8bb3d0ddc
Jeti Sumadinac <stefan.vukovic@terracore.tech>
2021-04-29 14:16:10 +0200

    video 31

---
+++ IdentityServer/Config.cs
@@ -0,0 +1,42 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client{ 
+                    ClientId = "movieClient",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { 
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "movieApi" }
+                }
+            };
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("movieApi","Movie API")
+            };
+        public static IEnumerable<ApiResource> ApiResources =>
+            new List<ApiResource>
+            {
+            };
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+            };
+        public static List<TestUser> TestUsers =>
+            new List<TestUser>
+            {
+            };
+    }
+}
